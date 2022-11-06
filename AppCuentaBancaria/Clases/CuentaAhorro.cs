a1cebae5175ee8b14d2d8a5f6839ebba0e06f6c0
Amne15 <amnellah@gmail.com>
2022-11-06 23:45:04 +0100

    Agregar archivos de proyecto.

---
+++ AppCuentaBancaria/Clases/CuentaAhorro.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCuentaBancaria.Clases
+{
+    internal class CuentaAhorro : Cuenta
+    {
+        //private Cuenta cuenta;
+        private int cuotaMantenimiento;
+        
+
+        /**/
+        public CuentaAhorro() : base()
+        {
+        }
+
+        /**/
+        public CuentaAhorro(int cuotaMantenimiento) : base()
+        {            
+            //this.cuenta = cuenta;
+            this.cuotaMantenimiento = cuotaMantenimiento;            
+        }
+
+        public CuentaAhorro(int numCuenta, string nombre, double saldo, string tipoInteres, int cuotaMantenimiento) : base(numCuenta, nombre, saldo, tipoInteres)
+        {
+            this.cuotaMantenimiento = cuotaMantenimiento;
+        }
+
+
+        /**/
+        public int getCuotaMantenimiento()
+        {
+            return this.cuotaMantenimiento;
+        }
+
+        /**/
+        public void setCuotaMantenimiento(int cuotaMantenimiento)
+        {
+            this.cuotaMantenimiento = cuotaMantenimiento;
+        }
+
+        public override string ToString()
+        {
+            //return base.ToString();
+            
+            return base.ToString() +", Cuota Mantenimiento: " + cuotaMantenimiento +" !!";
+        }
+    }
+}
---
+++ AppCuentaBancaria/Program.cs
@@ -0,0 +1,44 @@
+﻿using AppCuentaBancaria.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCuentaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Crea un objeto de la clase Cuenta.
+            Cuenta cuenta = new Cuenta();
+            
+            //Asigna un número de cuenta, un nombre, un saldo y un tipo de interés. 
+            cuenta.setNumCuenta(1);
+            cuenta.setNombre("Amnellah");
+            cuenta.setSaldo(1000);
+            cuenta.setTipoInteres("Deporte");
+
+            //Muestra el estado de dicha cuenta.
+
+            Console.WriteLine("ToString: "+cuenta.ToString());
+            Console.ReadKey();
+
+            //Crea un objeto de la clase CuentaAhorro.
+            CuentaAhorro cAhorro = new CuentaAhorro(0, "", 0, "", 0);
+            
+            //Asigna un número de cuenta, un nombre, un saldo, un tipo de interés y una cuota de mantenimiento. 
+            cAhorro.setNumCuenta(2);
+            cAhorro.setNombre("Laura");
+            cAhorro.setSaldo(1200);
+            cAhorro.setTipoInteres("Educacion");
+            cAhorro.setCuotaMantenimiento(200);
+
+            //Muestra el estado de dicha cuenta. 
+            Console.WriteLine("ToStringAhorro: " + cAhorro.ToString());
+            Console.ReadKey();
+        }
+    }
+}
